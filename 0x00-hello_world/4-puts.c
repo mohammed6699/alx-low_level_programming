899af95806c91cd1414bd52bf833558507097e03
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-10 14:34:40 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 # include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-char *text = "\"Programming is like building a multilingual puzzle" ;
+char *text ="\"Programming is like building a multilingual puzzle" ;
 puts("text");
 return (0);
 }
