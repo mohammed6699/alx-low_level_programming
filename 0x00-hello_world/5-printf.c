30656be5e59378260da31e3955eecb0e46757137
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-10 15:08:27 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+ * main - print line
+ *
+ * The basics of main function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+printf("ith proper grammar, but the outcome is a piece of art,");
+return (0);
+}
