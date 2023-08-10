d46a8bf33b21f44113df8010342645f5752b5b2f
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-10 15:48:18 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+# include <stdo.h>
+/**
+ *  main - print decimal
+ *
+ * The basics of main function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+printf("%d", sizeof(int));
+printf("%d", sizeof(char));
+printf("%d", sizeof(long));
+printf("%d", sizeof(long long int));
+printf("%d", sizeof(float));
+return (0);
+}
