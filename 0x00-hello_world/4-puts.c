2f9b7af4d6054840eee517049110908fdbbaa675
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-10 14:51:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 # include <stdio.h>
 /**
- * puts -  print, "Programming is like building a multilingual puzle
+ * This example demonstrates a basic example for main function
  *
+ * It print out "Programming is like building a multilingual puzzle
  * Return: Always 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
+
