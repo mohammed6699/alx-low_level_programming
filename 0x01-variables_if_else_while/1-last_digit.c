643b9d28c491eae3d8c3227198394d4f8c07d696
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-11 12:49:44 -0700

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,15 +20,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 5)
 {
-printf("Last digit of 98 is 8 and is greater than 5 \n");
+printf("Last digit of 98 is 8 and is greater than 5\n");
 }
 else if (n == 0)
 {
-printf("Last digit of 980 is 0 and is  0 \n");
+printf("Last digit of 980 is 0 and is  0\n");
 }
 else
 {
-printf("Last digit of -98 is -8 and is less than 6 and not 0 \n");
+printf("Last digit of -98 is -8 and is less than 6 and not 0\n");
 }
 return (0);
 }
