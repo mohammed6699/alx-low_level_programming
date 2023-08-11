776a5cfe2a7e2315e3a9973293228317061cb80e
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-11 16:56:54 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+/**
+ * main - print decimal
+ *
+ * The basics of main function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+putchar (ch)
+putchar ('\n')
+return (0);
+}
