31baa47edb46e0bac06ad71e0246a6e74103f04e
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-11 15:28:56 -0700

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ int main(void)
 {
 char ch1;
 char ch2;
-for (ch1 = 'z'; ch1 >= 'a'; ch1--)
+for (ch1 = 'a'; ch1 <= 'z'; ch1++)
 {
 putchar (ch1);
 }
-for (ch2 = 'a'; ch2 <= 'z'; ch2++)
+for (ch2 = 'z'; ch2 >= 'a'; ch2--)
 {
 putchar (ch2);
 }
