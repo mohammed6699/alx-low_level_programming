ab198d601f2e5aa242ce4e6010ebdb07ed761730
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-11 15:23:19 -0700

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+/**
+ * main - print alphapit
+ *
+ * The basic of main function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char ch1;
+char ch2;
+for (ch1 = 'z'; ch1 >= 'a'; ch1--)
+{
+putchar (ch1);
+}
+for (ch2 = 'a'; ch2 <= 'z'; ch2++)
+{
+putchar (ch2)
+}
+return (0);
+}
