c94318ac9bb7518562d33b790c874281a06e321e
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-11 14:23:15 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - putchar - print alphapit
+ *
+ *basic use for putchar
+ *
+ *Retairn: always 0 (seccess)
+ **/
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+return (0);
+}
