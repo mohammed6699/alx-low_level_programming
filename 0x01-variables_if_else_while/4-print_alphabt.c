6b966ac076ee0e704ca000525117804867d3e7e2
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-11 16:20:49 -0700

    alphap

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,18 +10,17 @@ int main(void)
 {
 char ch1;
 char ch2;
-for (ch1 = 'a'; ch1 >= 'p'; ch1++)
+for (ch1 = 'a'; ch1 <= 'p'; ch1++)
 {
 if (ch1 != 'e')
 {
 putchar (ch1);
 }
 }
-for (ch2 = 'r'; ch2 >= 'z'; ch2++)
+for (ch2 = 'r'; ch2 <= 'z'; ch2++)
 {
 putchar (ch2);
 }
 
 return (0);
 }
-
