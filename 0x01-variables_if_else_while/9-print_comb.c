2b7a59311c441c8e6080c54cd0ba67818098f658
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-11 18:22:00 -0700

    comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+/**
+ * main - print decimal
+ *
+ * The basics of main function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int ch1 = 0;
+while (ch1 < 10)
+{
+putchar (ch1 + '0');
+if (ch1 < 10)
+{
+putchar (',');
+putchar (' ');
+}
+ch1++;
+}
+putchar ('\n');
+return (0);
+}
