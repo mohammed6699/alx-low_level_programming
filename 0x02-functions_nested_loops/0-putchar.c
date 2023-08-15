5d12ac0cb27f827a98dd73b869f2eba70f2a8fa8
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-15 05:04:27 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+ * main - print line
+ *
+ * The example shows the basic uses for main funtion
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
