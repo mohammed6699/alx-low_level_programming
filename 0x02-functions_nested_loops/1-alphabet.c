e89e14539e8909d7e6e3edad08058095454993a9
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-15 06:12:52 -0700

    alphapet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+# include <main.h>
+/**
+ * main - check code
+ *
+ * Input - print_alphabet
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; c++)
+putchar (ch);
+
+}
+int main(void)
+{
+print_alphabet();
+return (0);
+}
