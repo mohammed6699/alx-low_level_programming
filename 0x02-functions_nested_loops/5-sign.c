e4d3fb96676274cbf5d70dcac000916dc6c49ec7
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-16 12:08:03 -0700

    sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		printf("+1");
 		return (1);
 	}
 	else if (n == 0)
@@ -21,7 +21,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		printf("-");
+		printf("-1");
 		return (-1);
 	}
 }
