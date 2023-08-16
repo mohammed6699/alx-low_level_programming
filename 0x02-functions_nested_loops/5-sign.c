cbfc2861d30502a4b18e2dff4606d022eac09197
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-16 12:01:21 -0700

    sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+# include "main.h"
+/**
+ *print_sign - print int
+ *
+ *@c: is the int to be printed
+ *
+ *Return: 1 for positive and 0 for zero and '-1' for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf("-");
+		return (-1);
+	}
+}
