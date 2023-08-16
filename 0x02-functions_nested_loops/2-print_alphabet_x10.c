b100ad8d3943d363685657c13dd798f4c601baa2
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-15 17:36:58 -0700

    alphapet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,10 @@
  * Return: Always 0 (success)
  */
 void print_alphabet_x10(void)
-{
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+ {
+      char ch;
+      for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
-putchar('\n');
-}
+ }
