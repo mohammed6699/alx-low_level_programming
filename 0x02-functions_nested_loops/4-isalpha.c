23b04bb82a23fa730725bd97af8599f5451702b5
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-16 11:02:03 -0700

    isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+/**
+ *int _isalpha
+ *
+ * Return: 1 for alphanetic and 0 for nothing
+ */
+int _isalpha(int c)
+{
+	if (c => 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c => 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else 
+	{
+	return (0);
+	}
+}	
