72827c004f073f2a09cc8bb2aecb6e3cb4bfb631
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-16 13:39:23 -0700

    abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+# include "main.h"
+/**
+ *int _abs(int)
+ *
+ *@n: the int to be calculated
+ *
+ * Return: -1*n for negative value and n for positive and 0
+ */
+int _abs(int)
+{
+	int n;
+	if (n < 0)
+	{
+		return (-1*n);
+	}
+	else
+	{
+		return n;
+	}
+
