4a49d819879ff671c0b09eb069de9ecc0df5ce0f
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-15 18:53:58 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,18 +3,20 @@
 /**
  * main - check code
  *
- * Print - print__alphabet
+ * Print - print c_value
  *
  * Return: Always 0 (success)
  */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z' )
+int c;
+if (c >= 'a' && c <= 'z')
 {
-return(1);
+return (1);
 }
 else
 {
 return (0);
 putchar(c);
+}
 }
