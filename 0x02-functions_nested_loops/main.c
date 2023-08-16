6924f6f094bf29c9b279b49129e802e97acc43e6
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-15 17:51:12 -0700

    alphapet

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,10 +1,9 @@
-/** #include "main.h"
-
-
+ #include "main.h"
+/**
  * main - check the code
  *
  * Return: Always 0.
- 
+ */ 
 int main(void)
 {
     print_alphabet_x10();
@@ -18,5 +17,5 @@ int main(void)
     print_alphabet_x10();
     print_alphabet_x10();
 	 return (0);
-}*/
+}
 
