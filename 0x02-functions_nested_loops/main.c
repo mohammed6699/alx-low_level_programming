2a31a7b2a236aa6777f557c2f8caa4be7e4da883
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-15 18:34:56 -0700

    islower

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,20 +1,27 @@
- #include "main.h"
+#include "main.h"
+
 /**
- * main - check the code
+ * test_islower - Test the _islower function
+ *
+ * @n: Number to pass to _islower function
+ */
+void test_islower(int n)
+{
+	int r;
+
+	r = _islower(n);
+	_putchar(r + '0');
+	_putchar('\n');
+}
+
+/**
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
- */ 
+ */
 int main(void)
 {
-int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	test_islower('H');
+	return (0);
 }
 
