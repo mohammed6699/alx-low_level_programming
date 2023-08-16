369a3045043c74cadffd0e669773cce5b50d34db
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-16 16:39:47 -0700

    24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+ *Print_minutes_in _day
+ *
+ *@i,hours,minutes: the int
+ *
+ *Return: 0 (success)
+ */
+void jack_bauer(void)
+{
+	int i;
+	int hours;
+	int minutes;
+	for (i = 0; i < 1440; i++)
+	{
+		hours = i/60;
+		minutes = i % 60;
+		printf ("%02d:%02d\n", hours, minutes);
+	}
+	return (0);
+}
