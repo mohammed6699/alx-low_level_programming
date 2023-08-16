802e272a5a0b2bd68115c2ee8ff80a43ae3a64d7
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-15 19:04:08 -0700

    islower

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,11 +8,11 @@ int main(void)
 {
 int c;
 c = _islower('H');
-_putchar(c + '0');
+putchar(c + '0');
 c = _islower('o');
-_putchar(c + '0');
+putchar(c + '0');
 c = _islower(108);
-_putchar(c + '0');
+putchar(c + '0');
 putchar('\n');
 return (0);
 }
