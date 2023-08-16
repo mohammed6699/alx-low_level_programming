cf0b258c89bd394a5ff5382f875074cd1afee8bd
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-16 15:51:53 -0700

    print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+ *print_last_digit
+ *
+ * @c: The number to be modefied
+ *
+ * Return: the absoulte number 
+ */
+int print_last_digit(int c)
+{
+c = c % 10;
+return (c);
+}
