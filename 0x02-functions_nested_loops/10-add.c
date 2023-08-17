f2ea3131067e896d385e9bd51c82b3cb6b8e215f
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-16 17:58:25 -0700

    add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+# include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int add(int, int)
+{
+	int a;
+	int b;
+	int result = a+b;
+	printf("%d", result);
+	return (1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int c);
 void jack_bauer(void);
+int add(int, int);
 #endif
