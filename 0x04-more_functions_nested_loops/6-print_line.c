6f11aa8886ed1492091f30570eb61919a13b5210
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-17 15:10:41 -0700

    print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+ *print_line - n is the number of times to print the line
+ *
+ *@i: the value to be obmited
+ *
+ * Return: 0 success
+ */
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		putchar('_');
+	}
+    putchar('\n');
+}
