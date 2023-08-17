bfe236e3da23f6419c2386060595f9a66966aced
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-17 16:44:43 -0700

    print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+# include "main.h"
+/**
+ * print_square - print aquare on terminal
+ *
+ * @i: the variable to be compared
+ *
+ * Return: 0 success
+ */
+void print_square(int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+
+	{
+		_putchar('#');
+	        _putchar('\n');
+	}
+}
