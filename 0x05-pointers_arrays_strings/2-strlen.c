4c5a8a3da41e322e89e24ec80a8f19d1d92a4d24
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-21 11:50:22 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+# include "main.h"
+/**
+ * _strlen - function to calculate
+ * The size of string
+ * *s is the pointer
+ * @  len: The length of string
+ * Return : len
+ */
+int _strlen(char *s)
+{
+int len = 0;
+while (*(s + len) != '\0')
+{
+	len++;
+}
+return (len);
+}
