2ea56106608b6dd1fd2f99993891b634d299036d
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-21 12:46:29 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+# include "main.h"
+/**
+ * void _puts(char *str) - function to print string
+ * *str - The pointer
+ *@len : The input
+ *Return: void
+ */
+void _puts(char *str)
+{
+	int len = 0;
+	while(*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+}
