cf65ce98555b20ce01f6f7381fd1c6839d889cad
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-21 15:21:20 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,13 +12,10 @@ void print_rev(char *s)
 	while(*s != '\0')
 	{
 		len++;
-		s++;
 	}
-	s--;
-	for(i = 0; i > 0; i--)
+	for(i = 0; i > 0; i++)
 	{
 		_putchar(*s);
-		s--;
 	}
 	_putchar('\n');
 }
