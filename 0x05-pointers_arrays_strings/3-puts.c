ce38d798e2f0cca7d91638c789a23754d347fb82
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-21 12:57:09 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,11 +7,10 @@
  */
 void _puts(char *str)
 {
-	int len = 0;
-	while (*(str + len) != '\0')
+	while (*str != '\0')
 
 	{
-		_putchar(*(str + len));
-		len++;
+		_putchar (*str++);
 	}
+	_putchar ('\n');
 }
