8d6eab0b11c2a8f3286ad507e7deadff91ca7249
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-21 08:08:29 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+ * reset_to_98(int *n) - function that takes a pointer
+ * update the value of its parameter
+ * @n : input
+ * return : n
+ */
+void reset_to_98(int *n)
+
+{
+int x = 98;
+*n = x;
+}
