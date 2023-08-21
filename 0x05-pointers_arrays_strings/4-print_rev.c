481933b748e5568e7359d8c39f0d52e9f5cfd911
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-21 15:42:02 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,20 +2,22 @@
 # include <string.h>
 /**
  * print_rev - function to print reverse string
- * @ len, i: inputs
+ * @ legthn, i, middle: inputs
+ * char: temp
+ * s: string
  * Return: void
  */
 void print_rev(char *s)
 {
-	int len = 0;
-	int i;
-	while(*s != '\0')
-	{
-		len++;
-	}
-	for(i = 0; i > 0; i++)
-	{
-		_putchar(*s);
-	}
-	_putchar('\n');
+int length = strlen(s);
+int middle = length / 2;
+int i;
+char temp;
+for (i = 0; i < middle; i++)
+{
+temp = s[i];
+s[i] = s[length - i - 1];
+s[length - i - 1] = temp;
+}
+_putchar('\n');
 }
