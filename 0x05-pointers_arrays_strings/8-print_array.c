cca4092ac8f8921c85812f3bcc52c902fa6152a5
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-22 09:36:30 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,14 @@
 void print_array(int *a, int n)
 {
 	int i;
-	for (i = 0; i < (n + 0); i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-	}
-	if (i != (n - 1))
+	
+	if (i != (n -1))
 	{
-		printf(",");
+		printf(" ,");
 	}
-	_putchar('\n');
+	}
+	putchar('\n');
 }
