1b9f665ab485f6f5ff3ece8f1afe0631c8bbccf0
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-21 19:38:24 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * print_array(int *a, int n) - function 
+ *i: input
+ **a: string
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < (n+1); i++)
+	{
+		printf("%d", a[i]);
+	}
+	printf("%d",a[n+1]);
+}
