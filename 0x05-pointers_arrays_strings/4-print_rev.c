94f8c0b93a6d901dfaeaca512ff71b07208e7d36
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-22 15:55:34 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,9 @@
  */
 void print_rev(char *s)
 {
-int length = strlen(s);
-int middle = length / 2;
-int i;
-char temp;
-for (i = 0; i < middle; i++)
-{
-temp = s[i];
-s[i] = s[length - i - 1];
-s[length - i - 1] = temp;
-}
+int i = 0;
+
+_putchar(s[i++]);
+_putchar(s[i--]);
 _putchar('\n');
 }
