ff2ab4e97e1663da4926df522548c4097a7807fb
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-24 14:21:47 -0700

    ./5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+# include "main.h"
+# include <string.h>
+# include <ctype.h>
+/**
+ * char *string_toupper - function to change
+ * lower charachter to upper character
+ * i: input
+ * s: string
+ * Return: o (success)
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	int length = strlen(s);
+	for (i = 0; i < length; i++)
+
+	s[i] = toupper(s[i]);
+	return 0;
+}
