97779c2b52314109a3d2094491c32a992ec97141
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-24 06:20:48 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+# include "main.h"
+/**
+ * char *_strcat - function to repeate string
+ * *dest: destination , *src: source
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int x = 0;
+int y = 0;
+
+while (src[x] != '\0')
+{
+	x++;
+}
+while (dest[y] != '\0')
+{
+	y++;
+}
+src[x] = dest[y];
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+# ifndef FUNCTIONS_H
+# define FUNCTIONS_H
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+# endif
