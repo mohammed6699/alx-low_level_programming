bcf371d95bc36518ae79227815852ddea399628b
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-24 09:07:06 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 # include "main.h"
 /***
- * @_strncpy - function to copy string
+ *  _strncpy - copy string
  * @dset: input
  * @src: input
  * @n: input
@@ -10,14 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int x = 0;
 
-while (x < n && src[x] != '\0')
-{
-x++;
-}
-while (x < n)
-{
-x++;
-}
-dest[x] = '\0';
+        while (x < n && src[x] != '\0')
+        {
+            src[x] == dest[x];
+            x++;
+        }
+        while (x < n)
+        {
+           
+            x++;
+        }        
 return (dest);
 }
