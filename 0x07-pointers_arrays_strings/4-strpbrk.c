38861406ca2d83dcc5e4fbcb0c7d4165d8a78379
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-28 15:04:57 -0700

    ./4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+# include "main.h"
+# include <string.h>
+/**
+ * *_strpbrk - function that searches a string for any of a set of bytes.
+ * *s, *accept: const char
+ * Return: (s)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *temp;
+
+	if (s == NULL || accept == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*s != '\0')
+	{
+		temp = accept;
+		while (*temp != '\0')
+		{
+			if (*s == *temp)
+			{
+				return (char *) s;
+			}
+			temp++;
+		}
+		s++;
+	}
+	return (s);
+}
