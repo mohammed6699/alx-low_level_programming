f64c034f33e52553659a3b374975855380e2eb13
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-28 12:12:32 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+# include "main.h"
+# include <string.h>
+/**
+ * *_strchr - function that locates a character in a string.
+ * i: input
+ * s: string
+ * c: charchter
+ * length: string length
+ * Return
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int length = strlen(s);
+
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (0);
+}
