22c3d00bd2238846c691ffdacd8e8049581e853b
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-08-30 16:36:24 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,17 +3,22 @@
 /**
  * _sqrt_recursion - function that calculate
  * The squre root for an input
- * squrt - function to cal the square root for inputs
  * @n: input
- * @m: input
- * Description: calculate the square root for an input
- * Return: (1) - true, (0) - false
+ * Description: return the input value 
+ * Return: (n)
  */
 int squrt(int n, int m);
 int _sqrt_recursion(int n)
 	{
 		return (squrt(n, 1));
 	}
+/**
+ * squrt - functio to calc yhe square root
+ * @n: input
+ * @m: input
+ * Dedcription: calculate the square root for an input
+ * Return: (m) - true, (-1) - faile
+ */
 int squrt(int n, int m)
 {
 	if (m * m == n)
