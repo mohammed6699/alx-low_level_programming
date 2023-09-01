625e125227d2f22d2e81605b2ba871f56b3d87df
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-01 12:10:00 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,29 +15,20 @@ int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
-	int num3 = atoi(argv[3]);
-	int num4 = atoi(argv[4]);
-	int num5 = atoi(argv[5]);
-	int sum = num1 + num2 + num3 + num4 + num5;
+	int sum = num1 + num2;
 
-	if (num1 == 'A' && num1 == 'Z')
+	if (*argv[1] == 'a' || *argv[2] == 'z')
 	{
 		printf("Error\n");
 		return(1);
 	}
-	else if(num2 == 'a' || num2 == 'z')
+	else if(argc <= 1)
 	{
-		printf("Error\n");
-		return(1);
-	}
-	else if(num1 == 0 || num2 == 0)
-	{
-		printf("%d\n", 0);
+		printf("%d\n", argc - 1);
 	}
 	else
 	{
 			printf("%d\n", sum);
 	}
-	(void)argc;
 	return(0);
 }
