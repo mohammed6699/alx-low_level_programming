c1d93bfc7ab0f18b3e293ec34357912c520b78c2
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-01 09:12:33 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 # include "main.h"
 # include <stdio.h>
 /**
- * int main - main function
- * @argc: argument count
+ * main - main function - that prints its name, followed by a new line
+ * @argc: argument count (void)
  * @argv: argument vector
+ * Description: program that prints its name, followed by a new line.
  * Return: o
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf ("%s\n", argv[0]);
-	return (0);
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
