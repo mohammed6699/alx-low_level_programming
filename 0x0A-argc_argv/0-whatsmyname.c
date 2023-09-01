7c8a889629f0610c5acfadc0ab957919b09449e6
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-01 07:36:30 -0700

    ./mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * int main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: o
+ */
+int main (int argc, char *argv[])
+{
+	printf ("argc: %d\n", argc);
+	printf ("argv[0] = %s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,4 @@
+# ifndef MAIN_H
+# define MAIN_H
+int man (int argc, char *argv[]);
+# endif
