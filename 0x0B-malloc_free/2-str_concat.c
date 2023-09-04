401727ded85fe9508acd73b96cdb827be44f9a47
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-04 13:39:46 -0700

    ./c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,12 +3,13 @@
 
 /**
  * *str_concat - function that concentates 2 string
- * @i: input
- * @x: input
- * @y: input
- * @n: input
- * *m: char
- * *s1: pointer, *s2: pointer
+ * i: input
+ * x: input
+ * y: input
+ * n: input
+ * m: char
+ * @s1: pointer
+ * @s2: pointer
  * Description: function that concentates 2 strings
  * Return: m
  */
@@ -24,10 +25,12 @@ if (s1 == NULL || s2 == NULL)
 {
 	return (NULL);
 }
-for (x = 0; s1[x] != '\0'; x++);
-for (y = 0; s2[y] != '\0'; y++);
+for (x = 0; s1[x] != '\0'; x++)
+;
+for (y = 0; s2[y] != '\0'; y++)
+;
 n = x + y + 1;
-m = (char*)malloc(n * sizeof(char));
+m = (char *)malloc(n *sizeof(char));
 if (m == NULL)
 return (NULL);
 for (i = 0; i < x; i++)
