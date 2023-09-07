57af12c9b9eeb4fb8a5504e73cde37cc533f049f
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-07 00:10:22 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,32 +9,42 @@
  * i: input
  * x: input
  * y: input
+ * k: size
  * Description: function that concatenates 2 strings
  * Return: string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	int x;
-	int y;
+	unsigned int i;
+	unsigned int k;
+	unsigned int x;
+	unsigned int y;
 	char *m;
 
 	if (s1 == NULL)
+		x = 0;
+	else
 	{
-		return (NULL);
-	}
 	for (x = 0; s1[x] != '\0'; x++)
 		;
+	}
+	if (s2 == NULL)
+		y = 0;
+	else
+	{
 	for (y = 0; s2[y] != '\0'; y++)
 		;
-	n = x + y + 1;
-	m = (char *)malloc(sizeof(char) * n);
+	}
+	if (y > n)
+		y = n;
+	k = x + y + 1;
+	m = (char *)malloc(sizeof(char) * k);
 	if (m == NULL)
 		return (NULL);
-	for (i = 0; i < x ; i++)
+	for (i = 0; i < x; i++)
 		m[i] = s1[i];
 	for (i = 0; i < y; i++)
-		m[x + i] = s2[i];
-	m[x + i] = '\0';
+		m[i + x] = s2[i];
+	m[x + y] = '\0';
 	return (m);
 }
