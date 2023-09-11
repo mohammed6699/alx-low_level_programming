1b635ece772d9d132ef081350c0867fd93860d8a
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-11 05:33:46 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef OBJECT_LIKE_MACRO_H_
+#define OBJECT_LIKE_MACRO_H_
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function return the name of the compiled file
+ *
+ * Return: (0) success
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
