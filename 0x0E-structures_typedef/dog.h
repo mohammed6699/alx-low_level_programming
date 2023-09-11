4bec0fd369ef8ac6b4fcc3faf5c55b2315a56164
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-11 06:16:33 -0700

    ./a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - atructure to input the dpg's info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
+ */
+struct dog
+{
 	char *name;
 	char *owner;
 	float age;
