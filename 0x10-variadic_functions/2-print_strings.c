231ec86a7202a4b3a987036beb808bda2dd0a272
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-14 07:44:18 -0700

    ./c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -38,7 +38,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", s);
 		}
 		if (i != (n - 1))
+		{
 			printf("%s", separator);
+		}
 	}
 	va_end(args);
 	printf("\n");
