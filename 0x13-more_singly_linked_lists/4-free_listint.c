7d507c702dd075898512a3b31afe885e404ee777
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-25 13:36:51 -0700

    ./e

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - function that delete the nodes
+ * @head: pointer
+ * rmp: new pointer to the node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *rmp = head;
+
+	while (rmp != NULL)
+	{
+		rmp = rmp->next;
+		free (head);
+		head = rmp;
+	}
+}
