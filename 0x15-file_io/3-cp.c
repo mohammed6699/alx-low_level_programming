3297a731dd62bc202e23350d50cb3e8d7ecd8425
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-10-03 08:09:54 -0700

    ./cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
-
 /**
  * main - check the code
- *
- * Return always 0
+ * @argc: no. of argc
+ * @argv: argument vector
+ * Return: 0 success
  */
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	char b[1024];
@@ -15,23 +13,21 @@ int main (int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf (2, "Usage: %s file_from file_to \n", argv[0]);
-		exit (97);
+		dprintf(2, "Usage: %s file_from file_to \n", argv[0]);
+		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
-
 	if (fd_from == -1)
 	{
-		dprintf (2, "Error: can't read from file %s\n", argv[1]);
-		exit (98);
+		dprintf(2, "Error: can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		dprintf (2, "Error: can't write to file %s\n", argv[2]);
-		exit (99);
+		dprintf(2, "Error: can't write to file %s\n", argv[2]);
+		exit(99);
 	}
 	while ((bytes_read = read(fd_from, b, sizeof(b) > 0)))
 	{
@@ -39,15 +35,15 @@ int main (int argc, char *argv[])
 		{
 			close(fd_from);
 			close(fd_to);
-			dprintf (2, "can't write to the file %s\n", argv[2]);
-			exit (99);
+			dprintf(2, "can't write to the file %s\n", argv[2]);
+			exit(99);
 		}
 	}
-	if (close (fd_from) == -1)
+	if (close(fd_from) == -1)
 	{
 		close(fd_to);
-		dprintf (2, "Error: can't close fd %d\n", fd_from);
-		exit (100);
+		dprintf(2, "Error: can't close fd %d\n", fd_from);
+		exit(100);
 	}
 	return (0);
 }
