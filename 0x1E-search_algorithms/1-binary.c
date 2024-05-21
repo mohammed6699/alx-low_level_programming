f6b66bd9126fb2b979609f16fdccc029afc465fd
root <mohammedahmed.refaat@gmail.com>
2024-05-21 16:24:12 -0700

    ./1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,1 +1,53 @@
 #include "search_algos.h"
+
+/*
+ * array: pointer to the first element of the array to search in
+ * size: the number of elements in array
+ * value: the value to search for
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	return (binary_search_recursive(array, 0, size - 1, value));
+}
+
+/**
+ * search_recursive: search recusivly for every element in array
+ * array: array
+ * low: first element
+ * high: last element
+ * value: the value we search for
+ */
+int binary_search_recursive(int *array, size_t low, size_t high, int value)
+{
+        size_t mid = low + (high - low)/2;
+        size_t i;
+
+        if (low > high)
+                return (-1);
+
+        printf("searching in array: ");
+
+        for  (i = low; i <= high; ++i)
+        {
+                printf("%d", array[i]);
+                if (i < high)
+                        printf(", ");
+        }
+        printf("\n");
+
+        if (array[mid] == value)
+        {
+                return (mid);
+        }
+        else if (array[mid] > value)
+        {
+                return (binary_search_recursive(array, low, mid - 1, value));
+        }
+        else
+        {
+                return (binary_search_recursive(array, mid + 1, high, value));
+        }
+}
+
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-/*linked list
- */
+
 
 /*prototypes*/
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_recursive(int *array, size_t low, size_t high, int value);
 #endif
