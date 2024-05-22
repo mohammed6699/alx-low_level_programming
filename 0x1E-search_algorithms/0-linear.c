59ad2bf6967c1df1586241e73659ff9d8b4e373d
root <mohammedahmed.refaat@gmail.com>
2024-05-22 09:15:39 -0700

    ./0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,10 +1,12 @@
 #include "search_algos.h"
-
-/*
- * linear_search: function to search in all the list
- * array: pointer to the first element of the array to search in
- * size: size of the array
- * value: value to search for
+/**
+ * linear_search - function to search in all the list and the index
+ * @array: pointer to the first element of the array to search in
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: if the lst is not present retrun NULL, -1.
+ * otherwie return the index for the element
  */
 
 int linear_search(int *array, size_t size, int value)
